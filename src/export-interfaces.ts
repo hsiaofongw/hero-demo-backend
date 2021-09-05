56f85695d29acc1476088232e7c568e6a5d88bf0
hsiaofongw <hsiaofong.w@gmail.com>
2021-09-06 00:15:22 +0800

    build: 把接口开放给前端用

--- src/export-interfaces.ts
+++ src/export-interfaces.ts
@@ -1,7 +1,16 @@
 /** 当此仓库作为一个 submodule 立足于前端仓库时，前端代码会引用此文件导出的 interfaces */
 
-import { IArticle } from './shared/content-provider/interfaces';
-import { ICard } from './shared/content-provider/interfaces';
+import {
+  IArticle,
+  IArticleQueryParam,
+  IArticleQueryResult,
+} from './shared/content-provider/interfaces';
 
-export { IArticle };
-export { ICard };
+import {
+  ICard,
+  ICardQueryParam,
+  ICardQueryResult,
+} from './shared/content-provider/interfaces';
+
+export { IArticle, IArticleQueryParam, IArticleQueryResult };
+export { ICard, ICardQueryParam, ICardQueryResult };
